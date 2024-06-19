bf1890e503a15716ba412c77723987909baa3e04
hs-hack <75413842+hs-hack@users.noreply.github.com>
2024-06-18 21:27:04 -0600

    final

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,12 +1,19 @@
 // ignore_for_file: library_private_types_in_public_api
 
 import 'dart:io' show Platform;
+import 'package:bitcoin_ticker/Network.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import './coin_data.dart';
 
+const apiKey = 'B6ADE6E4-6F6C-4F42-9F11-4CDB5B78B368';
+
 const List<String> currencyList = currenciesList;
+const List<String> crypto = cryptoList;
+
 String selectedCurrency = 'USD';
+String selectedCrypto = 'BTC';
+double? val;
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -28,7 +35,8 @@ class _PriceScreenState extends State<PriceScreen> {
         ],
         onChanged: (value) {
           setState(() {
-            selectedCurrency = value!;
+            getdata(value!);
+            selectedCurrency = value;
           });
         });
 //               // items: currencyList.map<DropdownMenuItem<String>>((String value) {
@@ -48,8 +56,24 @@ class _PriceScreenState extends State<PriceScreen> {
   CupertinoPicker appl() {
     return CupertinoPicker(
         itemExtent: 32.0,
-        onSelectedItemChanged: (value) {},
+        onSelectedItemChanged: (value) {
+          getdata(currenciesList[value]);
+        },
         children: [for (String i in currenciesList) Text(i)]);
+  }
+
+  getList(){
+    
+  }
+
+  Future<dynamic> getdata(String cur) async {
+    Network network = Network(
+        'https://rest.coinapi.io/v1/exchangerate/BTC/$cur?apikey=B6ADE6E4-6F6C-4F42-9F11-4CDB5B78B368');
+    var data = await network.getData();
+    double val1 = data['rate'];
+    String inst = val1.toStringAsFixed(2);
+    val = double.parse(inst);
+    return val;
   }
 
   @override
@@ -65,26 +89,56 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: crypto
+                .map(
+                  (e) => Padding(
+                    padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+                    child: Card(
+                      color: Colors.lightBlueAccent,
+                      elevation: 5.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 15.0, horizontal: 28.0),
+                        child: Text(
+                          '1 $e = $val $selectedCurrency ,$e',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
+                )
+                .toList(),
+            // Padding(
+            //   padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+            //   child: Card(
+            //     color: Colors.lightBlueAccent,
+            //     elevation: 5.0,
+            //     shape: RoundedRectangleBorder(
+            //       borderRadius: BorderRadius.circular(10.0),
+            //     ),
+            //     child: Padding(
+            //       padding: const EdgeInsets.symmetric(
+            //           vertical: 15.0, horizontal: 28.0),
+            //       child: Text(
+            //         '1 $i = $val $selectedCurrency',
+            //         textAlign: TextAlign.center,
+            //         style: const TextStyle(
+            //           fontSize: 20.0,
+            //           color: Colors.white,
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
           ),
           Container(
             height: 150.0,
